39fb2ca67c4724d0d31fdfa3254d7fc0d5993404
Kateryna Kraieva <70651413+ketryin@users.noreply.github.com>
2022-04-25 22:41:24 +0300

    adds message and date to list

--- src/components/FriendItem/FriendItem.js
+++ src/components/FriendItem/FriendItem.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Avatar from "../Avatar/Avatar.js";
+import moment from "moment";
 import "./FriendListItem.css";
 
-const FriendItem = ({ name, avatar }) => {
+const FriendItem = ({ name, avatar, message, date }) => {
   return (
     <div className="friend-conteiner">
       <Avatar name={name} avatar={avatar} />
       <div className="text-conteiner">
         <p className="friend-name">{name}</p>
-        <p className="friend-message">Message</p>
+        <p className="friend-message">{message}</p>
       </div>
-      <p className="message-date">Date</p>
+      <p className="message-date">
+        {date && moment(date).format("MMM D, YYYY")}
+      </p>
     </div>
   );
 };
